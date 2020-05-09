5e1812b5c84ddcea24df0287671a7fe271f39249
Sergey <komarovs33@mail.ru>
2020-05-09 11:15:29 +0300

    up

--- wordsCount.js
+++ wordsCount.js
@@ -1,6 +1,6 @@
 const R = require("ramda");
 
-const testString = "This is test string where i can consider words count";
+const testString = "This is string where i can consider words count";
 const explode = (str) => str.split(/\s+/);
 const count = (arr) => arr.length;
 const log = console.log;
